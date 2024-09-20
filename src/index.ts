32d94838f2d06745f396f4e3b4f819409c3ca672
gbgk <git@gbgk.me>
2024-09-20 14:02:45 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,83 @@ import bbaSubjects from './programmes/bba.mjs';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url).searchParams.get('url');
-		const icsData = await fetch(url!).then((res) => res.text());
-		console.log(icsData);
-		const jcalData = ical.parse(icsData);
-		const comp = new ical.Component(jcalData);
-		const vevents = comp.getAllSubcomponents('vevent');
+		const url = new URL(request.url);
+		if (url.pathname === '/') {
+			return new Response(
+				`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Rooster</title>
+</head>
+<body>
+  <p>Better UI coming soon</p>
 
-		comp.updatePropertyWithValue('SUMMARY', 'Lessons Schedule');
-		// X-WR-CALNAME
-		comp.updatePropertyWithValue('X-WR-CALNAME', 'Lessons Schedule');
+  <form id="calform">
+    <label for="url">Calendar URI:</label>
+    <input type="url" name="url" id="url" required>
+    <button type="submit">Get calendar</button>
+  </form>
 
-		vevents.forEach((vevent) => {
-			const event = new ical.Event(vevent);
+	<script>
+		document.getElementById('calform').addEventListener('submit', (e) => {
+			e.preventDefault();
+			const url = document.getElementById('url').value;
+			window.location.href = 'webcal://' + window.location.host + '/calendar?url=' + encodeURIComponent(url);
+		});
+		</script>
+</body>
+</html>`,
+				{
+					headers: {
+						'Content-Type': 'text/html',
+					},
+				}
+			);
+		} else if (url.pathname === '/calendar') {
+			const calendarURL = url.searchParams.get('url');
+			const icsData = await fetch(calendarURL!).then((res) => res.text());
+			console.log(icsData);
+			const jcalData = ical.parse(icsData);
+			const comp = new ical.Component(jcalData);
+			const vevents = comp.getAllSubcomponents('vevent');
 
-			const ectsCode = event.summary.split(' ')[0];
+			comp.updatePropertyWithValue('SUMMARY', 'Lessons Schedule');
+			// X-WR-CALNAME
+			comp.updatePropertyWithValue('X-WR-CALNAME', 'Lessons Schedule');
 
-			const subject = bbaSubjects.find((subject) => subject.ectsCode === ectsCode.toUpperCase());
+			vevents.forEach((vevent) => {
+				const event = new ical.Event(vevent);
 
-			const originalSummary = event.summary;
+				const ectsCode = event.summary.split(' ')[0];
 
-			event.summary = subject ? subject.subjectName : originalSummary.split(',')[0].split(' ')[1];
+				const subject = bbaSubjects.find((subject) => subject.ectsCode === ectsCode.toUpperCase());
 
-			if (subject) {
-				event.description = `${subject.teachers.map((teacher) => `${teacher.name}\n${teacher.profile}`).join('\n\n')}
+				const originalSummary = event.summary;
+
+				event.summary = subject ? subject.subjectName : originalSummary.split(',')[0].split(' ')[1];
+
+				if (subject) {
+					event.description = `${subject.teachers.map((teacher) => `${teacher.name}\n${teacher.profile}`).join('\n\n')}
 
 Original Summary:
 ${originalSummary}`;
-			}
-		});
+				}
+			});
 
-		const newJcalData = comp.toString();
+			const newJcalData = comp.toString();
 
-		return new Response(newJcalData, {
+			return new Response(newJcalData, {
+				headers: {
+					'Content-Type': 'text/calendar',
+				},
+			});
+		}
+
+		return new Response(`404`, {
 			headers: {
-				'Content-Type': 'text/calendar',
+				'Content-Type': 'text/html',
 			},
 		});
 	},
